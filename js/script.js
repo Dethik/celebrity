0179fc9ee09e04e3f064cb31b7c525889a87eef0
Austin Landis <austinlandiscode@gmail.com>
2021-01-14 14:03:59 -0800

    Add our perfect matches in html, finished the if elseif and else statements in js.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>
    Co-authored-by: Zack Rutledge <thorgrim88@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   $("form#dating").submit(function(event) {
+    event.preventDefault();
     const name = $("input#name").val();
-    const height = $("input#height").val();
-    const age = $("input#age").val();
-    const income = $("input#income").val();
-    const kids = $("input#kids").val();
+    const height = parseInt($("input#height").val());
+    const age = parseInt($("input#age").val());
+    const income = parseInt($("input#income").val());
+    const kids = parseInt($("input#kids").val());
     const divorce = $("input#divorce").val();
 
-    $(.)
-  })
-})
+    if (age >= 25 && age <=40 && height >= 72 && income >= 100000) {
+      $('#kimkardashian').show();
+    } else if (age <= 25 && height > 68 && kids === 0) {
+      $('#kyliejenner').show();
+    } else if (age >= 50 && name !== "Bruce" && name !== "Robert") {
+      $('#krisjenner').show();
+    } else {
+      $('#khloekardashian').show();
+    }
+  });
+});
