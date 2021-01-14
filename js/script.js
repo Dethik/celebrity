7a8d22322b9460c860b923800df7038ae914fd42
Austin Landis <austinlandiscode@gmail.com>
2021-01-14 13:45:20 -0800

    Add all of the css, and js links. Make variables in js.
    Co-authored-by: Zack Rutledge <thorgrim88@gmail.com>
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form#dating").submit(function(event) {
+    const name = $("input#name").val();
+    const height = $("input#height").val();
+    const age = $("input#age").val();
+    const income = $("input#income").val();
+    const kids = $("input#kids").val();
+    const divorce = $("input#divorce").val();
+
+    $(.)
+  })
+})
